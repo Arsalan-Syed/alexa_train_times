82fa53ed7206e1f8c7b7674cba1863984c91138b
Arsalan-Syed <arsalan.s96@gmail.com>
2020-02-29 13:41:20 +0000

    Deployment script works

--- deployment.bat
+++
@@ -1,7 +0,0 @@
-SET LAMBDA_NAME=tfl_status
-SET ZIP_FILE_NAME=code.zip
-
-pip install -r requirements.txt -t ./
-7z a -tzip %ZIP_FILE_NAME% @listfile.txt
-aws lambda update-function-code --profile personal --function-name %LAMBDA_NAME% --zip-file fileb://%ZIP_FILE_NAME%
-DEL %ZIP_FILE_NAME%
